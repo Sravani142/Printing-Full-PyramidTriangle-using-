1e7df7e9ff25f1e7fe58c3e51e0e60ca02a1826d
Sravani142 <84003407+Sravani142@users.noreply.github.com>
2021-05-15 18:43:39 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,24 @@
+class Patterns
+{
+public static void main(String args[])
+{
+     int n=3 , row, c ,temp;
+     temp = n;
+     for(int row=1;i<n;i++)
+     {
+        for(int c=1; c<temp; c++)
+
+           System.out.print("");
+
+           temp = temp-1;
+
+        for(int c=1;j<=2*row-1; c++)
+
+            System.out.print("*");
+
+      System.out.println();
+
+     }
+
+}
+}
